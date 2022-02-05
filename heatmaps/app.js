319316eafd9f83e57c8244fafc4906ba2f38f535
conner-brew <conner-brew@berkeley.edu>
2022-02-05 14:38:36 -0500

    Log Scale test

--- heatmaps/app.js
+++ heatmaps/app.js
@@ -34,7 +34,7 @@ async function draw(el, scale) {
   else if(scale === 'quantize') {
     colorScale = d3.scaleQuantize()
       .domain(d3.extent(dataset))
-      .range(['white', 'pink', 'red'])
+      .range(d3.schemeReds[5])
   }
 
   //The quantile scale automatically defines buckets for color division
@@ -42,14 +42,14 @@ async function draw(el, scale) {
   else if(scale === 'quantile') {
     colorScale = d3.scaleQuantile()
       .domain(dataset)
-      .range(['white', 'pink', 'red'])
+      .range(d3.schemeReds[5])
   }
 
   //The threshold scale allows the user to specify the distribution of buckets
   else if(scale === 'threshold') {
     colorScale = d3.scaleThreshold()
       .domain([45200, 135600])
-      .range(['white', 'pink', 'red'])
+      .range(d3.schemeReds[3])
   }
 
   //initialize rectangles
--- scales/app.js
+++ scales/app.js
@@ -1,22 +1,51 @@
+
 async function draw() {
-  // Data
+  //get data
   const dataset = await d3.json('data.json')
 
-  const sizeAccessor = (d) => d.size
-  const nameAccessor = (d) => d.name
+  const sizeAccessor = (d) => d.size 
+  const nameAccessor = (d) => d.name 
 
-  // Dimensions
+  //set dimensions
   let dimensions = {
     width: 200,
     height: 500,
     margin: 50
-  };
+  }
 
-  // Draw Image
+  //draw image by selecting html el and appending svg
   const svg = d3.select('#chart')
-    .append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height)
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
+
+  //create size scale
+  const universeScale = d3.scaleLog()
+    .domain(d3.extent(dataset, sizeAccessor))
+    .range([
+      dimensions.height - dimensions.margin,
+      dimensions.margin
+    ])
+
+  //append g
+  const circlesGroup = svg.append('g')
+
+  //draw items
+  circlesGroup.selectAll('circle')
+    .data(dataset)
+    .join('circle')
+    .attr('cx', dimensions.margin)
+    .attr('cy', d => universeScale(sizeAccessor(d)))
+    .attr('r', 6)
+
+  //draw labels
+  circlesGroup.selectAll('text')
+    .data(dataset)
+    .join('text')
+    .attr('x', dimensions.margin + 15)
+    .attr('y', d => universeScale(sizeAccessor(d)))
+    .text(nameAccessor)
+
 }
 
 draw()
